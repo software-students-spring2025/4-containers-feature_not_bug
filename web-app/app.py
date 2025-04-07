849180f5947ceb1d6b64b4c72fca40c300524002
mayhumst <mayahumston@gmail.com>
2025-04-07 16:23:54 -0400

    pylint + black done; added basic template

--- web-app/app.py
+++ web-app/app.py
@@ -1,52 +1,47 @@
-from flask import Flask, render_template, request, url_for, redirect, session
-#import pymongo
-from bson.objectid import ObjectId
-import database, filter
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
+"""import modules for flask app"""
+
+# import datetime
 import os
 import certifi
+from flask import Flask, render_template, request  # , url_for, redirect, session
+
+# import pymongo
+# from bson.objectid import ObjectId
+# import database, filter
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 from dotenv import load_dotenv
-from flask_session import Session
-import datetime
 
-'''
-notes / instructions
 
-run app.py, then go to 127.0.0.1:5000 in browser
-
-'''
-
-# load environment variables 
+# load environment variables
 load_dotenv()
-'''
+
 # connect MongoDB
 uri = os.getenv("MONGO_URI")
-client = MongoClient(uri, server_api=ServerApi('1'), tlsCAFile=certifi.where())
-Mongo_DBNAME= os.getenv("MONGO_DBNAME")
-myDb= client[Mongo_DBNAME]
-'''
+client = MongoClient(uri, server_api=ServerApi("1"), tlsCAFile=certifi.where())
+Mongo_DBNAME = os.getenv("MONGO_DBNAME")
+myDb = client[Mongo_DBNAME]
 
-app = Flask(__name__, static_folder='assets')
+app = Flask(__name__, static_folder="assets")
 
-# start new user session
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
 
-# homepage / dashboard
-@app.route("/", methods=('GET', 'POST'))
+@app.route("/", methods=("GET", "POST"))
 def show_dashboard():
-    
+    """
+    Show homepage / dashboard
+    """
+
+    data = {}
     # show the dashboard
-    if request.method == "GET":              
-        
+    if request.method == "GET":
         # get all necessary data
-        data = {
-            
-        }
-    elif request.method == "POST":
-        pass 
+        data = {"filler": "filler"}
 
-    return render_template('index.html') # render home page template 
+    return render_template("index.html", data=data)  # render home page template
 
+
+# keep alive
+if __name__ == "__main__":
+    app.run(
+        debug=True
+    )  # running your server on development mode, setting debug to True
