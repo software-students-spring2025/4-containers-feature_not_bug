11f970ecf221cfecea5f6f66ec748a1f781db5f1
mayhumst <mayahumston@gmail.com>
2025-04-07 18:57:27 -0400

    lint + black

--- web-app/tests/test_app.py
+++ web-app/tests/test_app.py
@@ -1,7 +1,10 @@
-from app import app # Flask instance of the API
+"""Module created to test the GoDutch Flask application"""
+
+from app import app  # Flask instance of the API
+
 
 def test_index_route():
-    response = app.test_client().get('/')
+    """Request the path '/' and ensure a 200 code response"""
+    response = app.test_client().get("/")
 
     assert response.status_code == 200
-    
