7d4bbd9a7f6443404dc74eeefa468dee7d672365
mayhumst <mayahumston@gmail.com>
2025-04-08 15:23:53 -0400

    import pytest

--- web-app/tests/test_app.py
+++ web-app/tests/test_app.py
@@ -1,10 +1,11 @@
 """Module created to test the GoDutch Flask application"""
 
-from app import app  # Flask instance of the API
+import pytest
+import app  # Flask instance of the API
 
 
 def test_index_route():
     """Request the path '/' and ensure a 200 code response"""
-    response = app.test_client().get("/")
+    response = app.app.test_client().get("/")
 
     assert response.status_code == 200
