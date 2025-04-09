bd7845995d50d7a957596259f5ca370a806d321c
mayhumst <mayahumston@gmail.com>
2025-04-09 00:30:29 -0400

    rename client again

--- web-app/tests/test_app.py
+++ web-app/tests/test_app.py
@@ -5,18 +5,18 @@ from app import app_setup  # Flask instance of the API
 
 
 @pytest.fixture
-def client_setup():
+def client():
     """
     Create and yield Flask app
     """
     app = app_setup()
     app.testing = True  # necessary for assertions to work correctly
-    with app.test_client() as client:
-        yield client
+    with app.test_client() as testing_client:
+        yield testing_client
 
 
-def test_index_route(client):
+def test_index_route(testing_client):
     """Request the path '/' and ensure a 200 code response"""
-    response = client.get("/")
+    response = testing_client.get("/")
 
     assert response.status_code == 200
